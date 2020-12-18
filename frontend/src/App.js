8bcfb6d138635031a0fb2ecd4f5a0696e805d8d8
Samip Poudel <sawmeep58@gmail.com>
2020-12-18 16:42:56 +0545

    created the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Proshop</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
